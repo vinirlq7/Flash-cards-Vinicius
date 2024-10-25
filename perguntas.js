ba6931026b921b8f94017aa4a5169b3393decfea
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2024-10-25 08:39:23 -0300

    25\10

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
-criaCartao4(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+criaCartao(
+    'Agro',
+    'Quaquantos CV tem o trator Valtra a74s?',
+    '76CV'
 )
 
-criaCartao4(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+criaCartao(
+    'agro',
+    'Qual o nome do maior trator do mundo?',
+    'Big Bud'
 )
 
-criaCartao4(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+criaCartao(
+    'agro',
+    'Qual o pais é o maior produtor de trigo no mundo?',
+    'China'
 )
 
-criaCartao4(
-    'Categoria',
-    'Pergunta?',
-    'Resposta'
+criaCartao(
+    'Agro',
+    'Qual o melhor produtor de soja do mundo?',
+    'Brasil'
 )
 
