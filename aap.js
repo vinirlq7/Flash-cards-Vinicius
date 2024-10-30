6e45864c153c59e0da5c02774e66b8a7dc9ce212
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2024-10-30 09:36:47 -0300

    30/10

--- aap.js
+++ aap.js
@@ -1,6 +1,6 @@
 function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
-    let cartao = document.createElement('article]');
+    let cartao = document.createElement('article');
     cartao.className = 'cartao';
 
     cartao.innerHTML = `
